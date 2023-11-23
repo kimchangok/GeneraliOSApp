a0fffce30700aeede628919ac387b55ad4772fb5
changok <chanok89@inswave.com>
2023-11-23 17:05:15 +0900

    initial commit

---
+++ DemoProj/DemoProj/DemoProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProjApp.swift
+//  DemoProj
+//
+//  Created by 김창옥 on 2023/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DemoProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
